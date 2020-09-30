d1a029146e4befb00e88de8275cd62a07f4501cd
AmiraMoussa-code <amira.moussa7197@gmail.com>
2020-09-30 10:53:54 +0300

    Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,9 @@
+document.getElementById("validate").onclick = function(a,b){
+
+var firstNumber = document.getElementById("first_number").value;
+var secondNumber =document.getElementById("second_number").value;
+
+var multiply = firstNumber * secondNumber;
+
+alert(multiply);
+}
