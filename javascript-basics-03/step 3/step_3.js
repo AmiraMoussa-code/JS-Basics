3d19bc14635b9397a9af0d7d103e326cc47eb487
AmiraMoussa-code <amira.moussa7197@gmail.com>
2020-10-25 03:00:54 +0200

    step 3

---
+++ javascript-basics-03/step 3/step_3.js
@@ -0,0 +1,13 @@
+var myDiv= document.getElementById('text');
+
+function green(){
+    myDiv.setAttribute('style', 'color : green');
+}
+
+function red(){
+    myDiv.setAttribute('style', 'color : red');
+}
+
+function blue(){
+    myDiv.setAttribute('style', 'color : blue');
+}
